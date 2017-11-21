20baa1f9fd5a4b554daca64c37780c67ccd72fa4
Marko Koron <koron.marko@gmail.com>
2017-11-21 22:25:48 +0100

    Styled 3D flipping fields

--- scripts.js
+++ scripts.js
@@ -1,12 +1,19 @@
-var board = document.getElementById('board');
+var board = document.getElementById("board");
 for (let field = 1; field <= 9; field++) {
-    board.appendChild(createField('-', field));
+  board.appendChild(createField("-", field));
 }
 
 function createField(value, id) {
-    let field =  document.createElement('div');
-    field.className = 'field';
-    field.id = id;
-    field.innerText = value;
-    return field;
+  let field = document.createElement("div");
+  let front = document.createElement("div");
+  let left = document.createElement("div");
+  field.className = "field";
+  front.className = "front";
+  left.className = "left";
+  field.appendChild(left);
+  field.appendChild(front);
+  console.log(field);
+  field.id = id;
+  //field.innerText = value;
+  return field;
 }
